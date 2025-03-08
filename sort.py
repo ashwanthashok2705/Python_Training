bf634fe29d7e75aae0ec0eba3980762395920bbc
ashwanthashok2705 <ashwanthashok2004@gmail.com>
2025-03-08 12:06:52 +0530

    Create sort.py

---
+++ sort.py
@@ -0,0 +1,6 @@
+inputlist = [1,2,3,4,3,2,4,5,6]
+print("List before sort:",inputlist)
+inputlist.sort()
+print("list after ascending sort:",inputlist)
+inputlist.sort(reverse=True)
+print("list after descending sort:",inputlist)
